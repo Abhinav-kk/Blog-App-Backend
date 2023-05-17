5bf21e6193eac3db6b0ca86969ffdcd39fd7ae7b
Abhinav K K <91086614+Abhinav-kk@users.noreply.github.com>
2023-05-17 10:25:22 +0400

    Blog App Backend Complete

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+
+// Import the routes
+const routes = require('./routes');
+
+const connectDB = require('./db');
+
+connectDB();
+
+app.use(cors());
+
+// Mount the routes on the app
+app.use('/', routes);
+
+// Start the server
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
